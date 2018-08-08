7c1ed4f07e18671087e9a19d1262fa10534f3d65
Pan Cheng <pchengeducation@hotmail.com>
2018-08-08 13:51:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution here:
+function driversWithRevenueOver(array, string){
+  const bigEarners = array.filter(function(driver){
+    if (driver.revenue > string){
+      return driver;
+    };
+  });
+  return bigEarners;
+}
