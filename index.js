5d1f2d38cdb0059cf562bcbe2d159d8896bbc8de
Pan Cheng <pchengeducation@hotmail.com>
2018-08-08 13:56:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,3 +17,14 @@ function driverNamesWithRevenueOver(array, string){
   });
   return bigEarners;
 }
+
+function exactMatch(drivers, object){
+  const key = Object.keys(name);
+  const pairs = [];
+  drivers.filter(function (driver){
+    if (driver[key[0]] === name[key[0]]){
+      pairs.push(driver.name);
+    }
+  });
+  return pairs;
+}
